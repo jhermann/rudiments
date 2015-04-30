00835b07ee817165c22a6f683a562334ffbcd922
jhermann <jh@web.de>
2015-04-30 23:05:05 +0200

    :construction: activate sphinx.ext.napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ if not on_rtd:
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage',
-    'sphinx.ext.ifconfig', 'sphinx.ext.viewcode',
+    'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
