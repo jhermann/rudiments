f606dc7ba3c5593de589342b71ce936bb7e5edb3
jhermann <jh@web.de>
2015-06-16 13:02:49 +0200

    :new: module 'pysupport' with import helpers

---
+++ src/rudiments/pysupport.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=bad-continuation
+""" Python helpers + magic.
+"""
+# Copyright ©  2015 Jürgen Hermann <jh@web.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import absolute_import, unicode_literals, print_function
+
+import os
+import imp
+import sys
+
+
+def import_name(modulename, name=None):
+    """ Import identifier ``name`` from module ``modulename``.
+
+        If ``name`` is omitted, ``modulename`` must contain the name after the
+        module path, delimited by a colon.
+
+        Parameters:
+            modulename (str): Fully qualified module name, e.g. ``x.y.z``.
+            name (str): Name to import from ``modulename``.
+
+        Returns:
+            object: Requested object.
+    """
+    if name is None:
+        modulename, name = modulename.rsplit(':', 1)
+    module = __import__(modulename, globals(), {}, [name])
+    return getattr(module, name)
+
+
+def load_module(modulename, modulepath):
+    """ Load a Python module from a path under a specified name.
+
+        Parameters:
+            modulename (str): Fully qualified mudule name, e.g. ``x.y.z``.
+            modulepath (str): Directory to scan for module.
+
+        Returns:
+            Loaded module.
+    """
+    if '.' in modulename:
+        modulepackage, modulebase = modulename.rsplit('.', 1)
+    else:
+        modulepackage = ''
+
+    imp.acquire_lock()
+    try:
+        # Check if module is already loaded
+        if modulename not in sys.modules:
+            # Find module on disk and try to load it
+            path, name = os.path.split(modulepath)
+            name = os.path.splitext(name)[0]
+            handle, path, info = imp.find_module(name, [path])
+            try:
+                # Load the module and put into sys.modules
+                module = imp.load_module(modulename, handle, path, info)
+                if modulepackage:
+                    setattr(sys.modules[modulepackage], modulebase, module)
+            finally:
+                # Make sure handle is closed properly
+                if handle:
+                    handle.close()
+    finally:
+        imp.release_lock()
+
+    return sys.modules[modulename]
---
+++ src/tests/test_pysupport.py
@@ -0,0 +1,59 @@
+# *- coding: utf-8 -*-
+# pylint: disable=wildcard-import, missing-docstring, no-self-use, bad-continuation
+""" Test «some_module».
+"""
+# Copyright ©  2015 Jürgen Hermann <jh@web.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import absolute_import, unicode_literals, print_function
+
+import os
+
+import pytest
+
+from rudiments import pysupport
+
+
+class ImportTests(object):
+
+    def test_import_name(self):
+        impname = pysupport.import_name("rudiments.pysupport", "import_name")
+        assert impname is pysupport.import_name
+
+    def test_import_name_failure(self):
+        with pytest.raises(AttributeError):
+            pysupport.import_name("rudiments.pysupport", "foo_bar_baz")
+        with pytest.raises(ImportError):
+            pysupport.import_name("foo.bar.baz", "import_name")
+
+    def test_import_name_unary(self):
+        impname = pysupport.import_name("rudiments.pysupport:import_name")
+        assert impname is pysupport.import_name
+
+    def test_import_name_unary_failure(self):
+        with pytest.raises(ValueError):
+            pysupport.import_name("rudiments.pysupport")
+
+    def test_load_module(self):
+        rootdir = os.path.dirname(__file__)
+        while rootdir != os.path.dirname(rootdir):
+            if os.path.exists(os.path.join(rootdir, "setup.py")):
+                break
+            rootdir = os.path.dirname(rootdir)
+
+        lm_setup = pysupport.load_module("rudiments._lm_setup", os.path.join(rootdir, "setup.py"))
+        from rudiments import _lm_setup
+
+        assert _lm_setup is lm_setup
+        assert 'project' in dir(lm_setup)
+        assert type(_lm_setup.project) is dict
